754f90c309cb2effc17328baa3b9d83cef0e43ef
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 16:06:54 +0000

    Added associations between user and friendships in user.rb, also added :friends, :requested_friends, :pending_friends scopes for custom query in friendships table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :friends, -> { where("status = 'accepted'") }, through: :friendships
+  has_many :requested_friends, -> { where("status = 'requested'") }, through: :friendships, source: :friend
+  has_many :pending_friends, -> { where("status = 'pending'") }, through: :friendships, source: :friend
 end
