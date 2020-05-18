0eec32eabffa737bff1a9a25b479330461ece997
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 15:26:32 +0000

    Added private method setup_friends to use as before_action in friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,10 @@
 class FriendshipsController < ApplicationController
+  before_action :setup_friends
+
+  private
+
+    def setup_friends
+      @user = current_user
+      @friend = User.find(params[:id])
+    end
 end
