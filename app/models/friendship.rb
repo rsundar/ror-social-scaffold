e204654f83f6a0835bb9f4a3255e9043b806cac0
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 14:57:25 +0000

    Added exists? method to check for existing friendships in friendships.rb

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,12 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User', foreign_key: :friend_id
 
   validates_presence_of :user_id, :friend_id
+
+  # Return true if the users are (possibly pending) friends.
+
+  def self.exists?(user, friend)
+    !Friendship.find_by_user_id_and_friend_id(user, friend).nil?
+  end
+
+  
 end
