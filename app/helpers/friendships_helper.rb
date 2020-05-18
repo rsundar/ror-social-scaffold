487fbde70d352e34e0e85c97054b8ea50eefd4f1
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 15:38:58 +0000

    Added frienship_requested? helper method in friendships_helper.rb

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,8 @@
 module FriendshipsHelper
+  # Return true if a friendship has been requested.
+
+  def friendship_requested?(user, friend)
+    friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
+    friendship.status == 'requested'
+  end
 end
