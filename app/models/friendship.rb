e6aa2177eb8e056f98e9b27d46731d689cb55080
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 15:03:08 +0000

    Added accept method in friendships.rb and private method accept_one_side to update the satus attribute in both records for a friendship to 'accepted'

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -20,4 +20,25 @@ class Friendship < ApplicationRecord
       end
     end
   end
+
+  # Accept a friend request.
+
+  def self.accept(user, friend)
+    Friendship.transaction do
+      accepted_at = Time.now
+      accept_one_side(user, friend, accepted_at)
+      accept_one_side(friend, user, accepted_at)
+    end
+  end
+
+  private
+
+    # Update the database with one side of an accepted friendship request.
+
+    def self.accept_one_side(user, friend, accepted_at)
+      request = Friendship.find_by_user_id_and_friend_id(user, friend)
+      request.status = 'accepted'
+      request.accepted_at = accepted_at
+      request.save!
+    end
 end
