98c1e643e77ded2f75bdf858bd395c148a1de4a2
Helio Diniz <helio.diniz74@gmail.com>
2020-05-18 15:40:50 +0000

    Added friendship_pending? helper method in friendships_helper.rb

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -5,4 +5,11 @@ module FriendshipsHelper
     friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
     friendship.status == 'requested'
   end
+
+  # Return true if a friendship is pending
+
+  def friendship_pending?(user, friend)
+    friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
+    friendship.status == 'pending'
+  end
 end
