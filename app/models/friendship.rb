7cd580b1b8232d13e196e3d17434eb64b8eec602
rsundar <rohan.s.1987@gmail.com>
2020-05-21 22:43:53 +0530

    Finished Friendship model with associations and status indicatior

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,19 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: "User", foreign_key: 'friend_id'
+    belongs_to :user,
+    belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
+    
+    validates_presence_of :user_id, :friend_id
 
-  validates_presence_of :user_id, :friend_id 
+    def self.exists? (user, friend)
+        !find_by_user_id_and_friend_id(user, friend).nil?
+    end
+
+    def self.request(user, friend)
+        unless user == friend || Friendship.exists?(user, friend)
+            Friendship.transaction do
+                Friendship.create(user: user, friend: friend, status: 'pending')
+                Friendship.create(user: friend, friend: user, status: 'requested')
+            end
+        end
+    end
 end
