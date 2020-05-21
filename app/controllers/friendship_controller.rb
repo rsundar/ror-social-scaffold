5164e8c62c79a38726dcec397bca4ed2e71db30d
rsundar <rohan.s.1987@gmail.com>
2020-05-21 21:48:46 +0530

    completed friendship controller

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,10 +1,20 @@
 class FriendshipController < ApplicationController
+  before_action :setup
+  
   def create
+
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+
+  def setup
+    @user = current_user
+    @friend = params.find(params[:id])
   end
 end
