2da6d4fd786b82106a5ef549de311e15ca23f676
rsundar <rohan.s.1987@gmail.com>
2020-05-21 22:19:11 +0530

    added foreign keys for the friendship association

--- db/migrate/20200521164133_create_friendships.rb
+++ db/migrate/20200521164133_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.references :user_id, foreign_key: true
-      t.references :friend_id, foreign_key: true
+      t.references :user_id, null: false, foreign_key: { to_table: :users }
+      t.references :friend_id, null: false, foreign_key: { to_table: :users }
       t.boolean :status
 
       t.timestamps
