be9d2a72f2518a1288fda728b59283aefd69acdb
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-25 12:23:17 -0600

    Add test for signed_in user to visit homepage

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-feature "unsigned user visits homepage" do
-  scenario "succesfully" do
+feature "user visits homepage" do
+  scenario "unsigned user visits homepage" do
     visit root_path
 
     expect(page).to have_css 'p', text: 'You need to sign in or sign up before continuing.'
+  end
+
+  scenario "signed_in user visits homepage" do
+    user = User.create!(name: 'test', :email => 'test2@example.com', :password => 'f4k3p455w0rd')
+    login_as(user, :scope => :user)
+    visit root_path
+
+    expect(page).to have_css 'h3', text: 'Recent posts'
   end
 end
