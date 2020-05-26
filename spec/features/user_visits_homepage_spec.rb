b0de9e370be98cee2af3a3a43a5b5efa68647cf4
rsundar <rohan.s.1987@gmail.com>
2020-05-27 01:44:56 +0530

    passed the test

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -4,6 +4,6 @@ feature "User visits homepage" do
     scenario "Without logging in or signing up" do
         visit root_path
 
-        expect(page).to have_css 'p', text:'Please log in or sign up to continue!'
+        expect(page).to have_css 'p', text:'You need to sign in or sign up before continuing.'
     end
 end
