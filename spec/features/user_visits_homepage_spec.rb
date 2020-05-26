dc4934d8d47979bf2cad40d0f8790c9345bc1324
rsundar <rohan.s.1987@gmail.com>
2020-05-27 01:35:04 +0530

    changed scenario

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "User visits homepage" do
-    scenario "successfully" do
+    scenario "Without logging in or signing up" do
         visit root_path
 
         expect(page).to have_css 'p', text:'Please log in or sign up to continue!'
