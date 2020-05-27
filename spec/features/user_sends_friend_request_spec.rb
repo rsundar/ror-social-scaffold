cdbfb85f9f3162e48bc4e022a46e33a07b07eaaf
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-27 13:43:32 -0600

    Added minor refactor to feature tests

--- spec/features/user_sends_friend_request_spec.rb
+++ spec/features/user_sends_friend_request_spec.rb
@@ -1,31 +1,27 @@
 require 'rails_helper'
 
 feature "user sends friend request" do
+  before(:each) do
+    @user = create(:user)
+    @other_user = create(:user)
+    login_as(@user, :scope => :user)
+  end
+
   scenario "when friendship doesn't exist a button to send friend request appears" do
-    user = create(:user)
-    other_user = create(:user)
-    login_as(user, :scope => :user)
     visit users_path
     click_on 'Send friend request'
-
     expect(page).to have_content 'Friend request sent.'
   end
 
-  scenario "user can't see a button to send friend request to himself" do
-    user = create(:user)
-    login_as(user, :scope => :user)
+  scenario "when friendships exists, user can't see button to send friend request" do
+    Friendship.request(@user, @other_user)
     visit users_path
-
     expect(page.has_button? 'Send friend request').to be false
   end
 
-  scenario "when friendships exists, user can't see button to send friend request" do
-    user = create(:user)
-    other_user = create(:user)
-    login_as(user, :scope => :user)
-    Friendship.request(user, other_user)
+  scenario "user can't see a button to send friend request to himself" do
+    @other_user.destroy
     visit users_path
-    
     expect(page.has_button? 'Send friend request').to be false
   end
 end
