292094f85dd88c3d537d52db7d69a869f12213c0
rsundar <rohan.s.1987@gmail.com>
2020-05-28 04:05:47 +0530

    Removed more lint errors from friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: :friend_id
-    
+  
   validates_presence_of :user_id, :friend_id
 
-  def self.exists? (user, friend)
+  def self.exists?(user, friend)
     !find_by_user_id_and_friend_id(user, friend).nil?
   end
 
@@ -32,8 +32,8 @@ class Friendship < ApplicationRecord
     end
   end
 
-  private
-    
+  protected
+
   def self.accept_one_side(user, friend, accepted_at)
     request = find_by_user_id_and_friend_id(user, friend)
     request.status = 'accepted'
