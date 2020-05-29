87d639fd4f309917363be7c242d20f5e63bdc324
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-29 09:54:57 -0600

    refactored friendship helper method, 3 tests failing

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -9,4 +9,26 @@ module FriendshipsHelper
     friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
     friendship.status == 'pending'
   end
+
+  def friendship_buttons(user, friend)
+    return if user == friend
+
+    unless Friendship.exists?(user, friend)
+      return button_to "Send friend request", friendships_path, params: { id: friend.id }, method: :post, class: ' btn-primary'
+    end
+
+    friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
+    case friendship
+    when friendship.status == 'requested'
+      button_to "Confirm friendship", accept_path, params: { id: friend.id }, method: :patch, class: '  btn-primary'
+      button_to "Decline friendship", decline_path, params: { id: friend.id }, method: :delete, class: '  btn-primary'
+    when friendship.status == 'pending'
+      button_to "Cancel friend request", cancel_path, params: { id: friend.id }, method: :delete, class: '  btn-primary'
+    when friendship.status == 'accepted'
+      button_to "Remove friendship", delete_path, params: { id: friend.id }, method: :delete, class: ' btn-primary'
+    end
+
+      
+
+  end
 end
