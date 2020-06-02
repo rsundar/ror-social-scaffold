b9a897356335bb18a85f5783e25b11d2469c9ac1
Mario Barrios <mariobarriossalazar@outlook.com>
2020-06-02 13:12:10 -0600

    Add code to seeds.rb to populate the database and modify users show action to display posts only if friendship with the current user is accepted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,21 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.ordered_by_most_recent.includes(:user)
+    @posts = is_friendship_accepted?(current_user, @user) ? @user.posts.ordered_by_most_recent.includes(:user) : "private_profile"
   end
 
   def friend_requests
     @friend_requests = current_user.requested_friends
+  end
+
+  private
+
+  def is_friendship_accepted?(current_user, user)
+    return true if current_user == user
+
+    friendship = Friendship.find_by_user_id_and_friend_id(current_user, user)
+    return false if friendship.nil? || friendship.status != 'accepted'
+
+    return friendship.status == 'accepted'
   end
 end
