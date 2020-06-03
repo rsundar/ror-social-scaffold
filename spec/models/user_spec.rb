caf34c03f31eddb6aa5e412c8e65da098423b357
Mario Barrios <mariobarriossalazar@outlook.com>
2020-06-03 09:51:08 -0600

    Add test for friends scope in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,23 +7,31 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:password) }
   end
 
-  context "tests for friendship scopes" do
+  context "tests for user friendship scopes" do
     let!(:user) { create(:user) }
-    let!(:friend) { create(:user) }
+    let!(:other_user) { create(:user) }
 
     describe ":pending_friends scope" do
       it "includes friends that have a pending friendship status" do
-        Friendship.request(user, friend)
-        expect(user.pending_friends.include?(friend)).to be true
+        Friendship.request(user, other_user)
+        expect(user.pending_friends.include?(other_user)).to be true
       end
     end
 
     describe ":requested_friends scope" do
       it "includes friends that have requested friendship status" do
-        Friendship.request(user, friend)
-        expect(friend.requested_friends.include?(user)).to be true
+        Friendship.request(user, other_user)
+        expect(other_user.requested_friends.include?(user)).to be true
+      end
+    end
+
+    describe ":friends scope" do
+      it "includes friends that have accepted a friendship" do
+        Friendship.request(user, other_user)
+        Friendship.accept(user, other_user)
+        expect(user.friends.include?(other_user)).to be true
+        expect(other_user.friends.include?(user)).to be true
       end
     end
   end
-
 end
