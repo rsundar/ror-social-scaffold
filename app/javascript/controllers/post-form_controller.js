c920946eb04722ec8605da017ce9a9d15d9dd7d0
rsundar <rohan.s.1987@gmail.com>
2020-06-13 02:18:01 +0530

    Added character count indicator to the posts form.

---
+++ app/javascript/controllers/post-form_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = [ "body", "characterCount" ]
+
+    initialize() {
+      this.update()
+    }
+  
+    update() {
+      var count = this.bodyTarget.value.length
+      
+      var message = `${count} characters`
+      
+  
+      this.characterCountTarget.textContent = message
+  
+      if (count > 900) {
+        this.characterCountTarget.classList.remove("has-text-success")
+        this.characterCountTarget.classList.add("has-text-weight-bold has-text-danger")
+      } 
+      else {
+        this.characterCountTarget.classList.add("has-text-success")
+      }
+    }
+  
+    submit(event) {
+      var count = this.bodyTarget.value.length
+      if (count > 1000) {
+        event.preventDefault()
+      }
+    }
+}
